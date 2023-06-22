7a9df0017c1ffe7d1825454756beb9b6fa98380c
John <nazar.kusnul.1805125@students.um.ac.id>
2023-06-22 03:13:57 -0700

    All tasks done

--- push.c
+++ push.c
@@ -1,11 +1,10 @@
 #include "monty.h"
-
 /**
- * f_push - Adds a node to the stack
- * @head: Stack head
- * @counter: Line number
- * Return: No return value
- */
+ * f_push - add node to the stack
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+*/
 void f_push(stack_t **head, unsigned int counter)
 {
 	int n, j = 0, flag = 0;
@@ -16,31 +15,23 @@ void f_push(stack_t **head, unsigned int counter)
 			j++;
 		for (; bus.arg[j] != '\0'; j++)
 		{
-			if (bus.arg[j] > '9' || bus.arg[j] < '0')
-				flag = 1;
-		}
+			if (bus.arg[j] > 57 || bus.arg[j] < 48)
+				flag = 1; }
 		if (flag == 1)
-		{
-			fprintf(stderr, "L%u: usage: push integer\n", counter);
+		{ fprintf(stderr, "L%d: usage: push integer\n", counter);
 			fclose(bus.file);
 			free(bus.content);
 			free_stack(*head);
-			exit(EXIT_FAILURE);
-		}
-	}
+			exit(EXIT_FAILURE); }}
 	else
-	{
-		fprintf(stderr, "L%u: usage: push integer\n", counter);
+	{ fprintf(stderr, "L%d: usage: push integer\n", counter);
 		fclose(bus.file);
 		free(bus.content);
 		free_stack(*head);
-		exit(EXIT_FAILURE);
-	}
-
+		exit(EXIT_FAILURE); }
 	n = atoi(bus.arg);
-	if (bus.lifo == 0)
+	if (bus.lifi == 0)
 		addnode(head, n);
 	else
 		addqueue(head, n);
 }
-
